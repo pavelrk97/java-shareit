1b9e243d351b0612d37fecb4278132c88f7d70e2
Pavel <pavel_r_k9@mail.ru>
2025-07-01 00:18:20 +0300

    Start

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.model;
 
+import org.springframework.web.context.request.RequestScope;
+import ru.practicum.shareit.request.ItemRequest;
+import user.Userl
+
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean available;
+    private User owner;
+    private ItemRequest scope;
 }
