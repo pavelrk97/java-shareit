03415ed7705a816fa4aac4172507d2a31be370f6
Pavel <pavel_r_k9@mail.ru>
2025-07-18 09:06:07 +0300

    Start

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,7 +1,12 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDto;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-bookings.
@@ -9,4 +14,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/bookings")
 public class BookingController {
+    @RestController
+    @RequestMapping("/bookings")
+    @RequiredArgsConstructor
+    @Slf4j
+    public static class BookingController {
+
+        private final BookingService bookingService;
+
+        @PostMapping
+        public BookingDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                 @Valid @RequestBody BookingCreateDto bookingCreateDto) {
+            log.info("POST запрос на создание нового бронирования вещи от пользователя c id: {} ", userId);
+            return bookingService.create(userId, bookingCreateDto);
+        }
+
+        @PatchMapping("/{bookingId}")
+        public BookingDto update(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                 @PathVariable("bookingId") Long bookingId,
+                                 @RequestParam(name = "approved") Boolean approved) {
+            return bookingService.update(userId, bookingId, approved);
+        }
+
+        @GetMapping("/{bookingId}")
+        public BookingDto findBookingById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                          @PathVariable("bookingId") Long bookingId) {
+            log.info("GET запрос на получение данных о  бронировании от пользователя с id: {}", userId);
+            return bookingService.getBookingById(userId, bookingId);
+        }
+
+        @GetMapping
+        public List<BookingDto> findAll(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                        @RequestParam(value = "state", defaultValue = "ALL") String bookingState) {
+            log.info("GET запрос на получение списка всех бронирований текущего пользователя с id: {} и статусом {}", userId, bookingState);
+            return bookingService.findAll(userId, bookingState);
+        }
+
+        @GetMapping("/owner")
+        public List<BookingDto> getAllOwner(@RequestHeader("X-Sharer-User-Id") Long ownerId,
+                                            @RequestParam(value = "state", defaultValue = "ALL") String bookingState) {
+            log.info("GET запрос на получение списка всех бронирований текущего владельца с id: {} и статусом {}", ownerId, bookingState);
+            return bookingService.getOwnerBookings(ownerId, bookingState);
+        }
+    }
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,20 +1,30 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
 
 
 @Data
 @Builder
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "items")
 public class Item {
-    private Long id;
-    private String name;
-    private String description;
-    private Boolean available;
-    private Long owner;
-    private Long request;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(name = "name", nullable = false)
+    String name;
+    @Column(name = "description", nullable = false)
+    String description;
+    @Column(name = "is_available", nullable = false)
+    Boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    User owner;
 }
